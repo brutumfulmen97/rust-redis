0deac0069b1632d0ec5d8338043a81c4f754762f
Vlatko Percic <vlatko@Vlatkos-MacBook-Pro.local>
2026-07-18 13:49:57 +0200

    tangent: mini-tokio impl

---
+++ examples/mini-tokio.rs
@@ -0,0 +1,153 @@
+use futures::task::{self, ArcWake};
+use std::{
+    pin::Pin,
+    sync::{Arc, Mutex, mpsc},
+    task::{Context, Poll, Waker},
+    thread,
+    time::{Duration, Instant},
+};
+
+fn main() {
+    let mut mini_tokio = MiniTokio::new();
+
+    mini_tokio.spawn(async {
+        let when = Instant::now() + Duration::from_millis(10);
+        let future = Delay::new(when);
+
+        let out = future.await;
+        assert_eq!(out, ());
+        std::process::exit(0);
+    });
+
+    mini_tokio.run();
+}
+
+struct MiniTokio {
+    scheduled: mpsc::Receiver<Arc<Task>>,
+    sender: mpsc::Sender<Arc<Task>>,
+}
+
+// type Task = Pin<Box<dyn Future<Output = ()> + Send>>;
+
+impl MiniTokio {
+    fn new() -> Self {
+        let (sender, scheduled) = mpsc::channel();
+        Self { scheduled, sender }
+    }
+
+    fn spawn<F>(&mut self, future: F)
+    where
+        F: Future<Output = ()> + Send + 'static,
+    {
+        Task::spawn(future, &self.sender);
+    }
+
+    fn run(&mut self) {
+        while let Ok(task) = self.scheduled.recv() {
+            task.poll();
+        }
+    }
+}
+
+struct TaskFuture {
+    future: Pin<Box<dyn Future<Output = ()> + Send>>,
+    poll: Poll<()>,
+}
+
+impl TaskFuture {
+    fn new(future: impl Future<Output = ()> + Send + 'static) -> Self {
+        TaskFuture {
+            future: Box::pin(future),
+            poll: Poll::Pending,
+        }
+    }
+
+    fn poll(&mut self, cx: &mut Context<'_>) {
+        if self.poll.is_pending() {
+            self.poll = self.future.as_mut().poll(cx);
+        }
+    }
+}
+
+struct Task {
+    task_future: Mutex<TaskFuture>,
+    executor: mpsc::Sender<Arc<Task>>,
+}
+
+impl Task {
+    fn schedule(self: &Arc<Self>) {
+        let _ = self.executor.send(self.clone());
+    }
+
+    fn poll(self: Arc<Self>) {
+        let waker = task::waker(self.clone());
+        let mut cx = Context::from_waker(&waker);
+
+        let mut task_future = self.task_future.try_lock().unwrap();
+
+        task_future.poll(&mut cx);
+    }
+
+    fn spawn<F>(future: F, sender: &mpsc::Sender<Arc<Task>>)
+    where
+        F: Future<Output = ()> + Send + 'static,
+    {
+        let task = Arc::new(Task {
+            task_future: Mutex::new(TaskFuture::new(future)),
+            executor: sender.clone(),
+        });
+
+        let _ = sender.send(task);
+    }
+}
+
+impl ArcWake for Task {
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        arc_self.schedule();
+    }
+}
+
+struct Delay {
+    when: Instant,
+    waker: Option<Arc<Mutex<Waker>>>,
+}
+
+impl Delay {
+    fn new(when: Instant) -> Self {
+        Self { when, waker: None }
+    }
+}
+
+impl Future for Delay {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if Instant::now() >= self.when {
+            println!("Ready");
+            return Poll::Ready(());
+        }
+
+        if let Some(waker) = &self.waker {
+            let mut waker = waker.lock().unwrap();
+            if !waker.will_wake(cx.waker()) {
+                *waker = cx.waker().clone();
+            }
+        } else {
+            let when = self.when;
+            let waker = Arc::new(Mutex::new(cx.waker().clone()));
+            self.waker = Some(waker.clone());
+
+            thread::spawn(move || {
+                let now = Instant::now();
+                if now < when {
+                    thread::sleep(when - now);
+                }
+
+                let waker = waker.lock().unwrap();
+                waker.wake_by_ref();
+            });
+        };
+
+        Poll::Pending
+    }
+}
