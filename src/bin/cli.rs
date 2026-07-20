226ce1d8abaf82b96d3fb38cb11b05b7f5f5049d
Vlatko Percic <vlatko@Vlatkos-MacBook-Pro.local>
2026-07-20 20:54:07 +0200

    simple cli for testing commands

---
+++ src/bin/cli.rs
@@ -0,0 +1,56 @@
+use clap::{Parser, Subcommand};
+use rust_redis::client::Client;
+
+#[derive(Parser, Debug)]
+#[command(name = "rust-redis-cli", version, about = "Issue redis commands")]
+struct Cli {
+    #[clap(subcommand)]
+    command: Command,
+
+    #[arg(long, default_value = "127.0.0.1")]
+    host: String,
+
+    #[arg(long, default_value_t = 6379)]
+    port: u16,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    Get { key: String },
+    Set { key: String, value: String },
+    Del { keys: Vec<String> },
+    Ping { msg: Option<String> },
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> rust_redis::Result<()> {
+    let cli = Cli::parse();
+    let addr = format!("{}:{}", cli.host, cli.port);
+    let mut client = Client::connect(&addr).await?;
+
+    match cli.command {
+        Command::Get { key } => {
+            if let Some(value) = client.get(&key).await? {
+                if let Ok(s) = std::str::from_utf8(&value) {
+                    println!("\"{s}\"");
+                } else {
+                    println!("{value:?}");
+                }
+            } else {
+                println!("(nil)");
+            }
+        }
+        Command::Set { key, value } => {
+            client.set(&key, value.into()).await?;
+            println!("OK");
+        }
+        Command::Del { keys } => {
+            println!("not implemented yet");
+        }
+        Command::Ping { msg } => {
+            println!("not implemented yet");
+        }
+    }
+
+    Ok(())
+}
